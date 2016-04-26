35b0b8a5d8c5040067e7233cdfaf84fe8c4e8ea0
Intesis <izcmtzjose@gmail.com>
2016-04-26 13:41:10 -0500

    prueba

--- mail/MandaCorreos.php
+++ mail/MandaCorreos.php
@@ -1,16 +1,19 @@
 <?php
+	echo "hola";
+	
 	require 'vendor/autoload.php';
 	use Parse\ParseClient;
 	use Parse\ParseQuery;
 	ParseClient::initialize('cC2UDoJuBtf19dZYwzns0ifp04jx8U353J3lOmb2', '7Lq7uQfoJfnsPmJCxcBTVytfOZrecVjUpyoNwlP3', 'VaTxoMQF85SHDLyvQoiArprBKnyTCRnuLJiWfiLr');
 	use Parse\ParseObject;
 
-	//////////////////////////
+	echo "hola";//////////////////////////
 	//allow_url_include =true;
 	include 'mandarMetodo.php';
 	
 	$a = new mandar();
 	$mail=array();
+		echo "hola";
 	
 	$query = new ParseQuery("lista");
 	$query->equalTo("mandado", "no");
@@ -20,9 +23,13 @@
 		echo $mail[$i]=$object->get('mail');
 	}
 	
+		echo "hola";
+	
 	for($z=0;$z<count($mail); $z++){
 		echo $b = $a->Send($mail[$z]);
 	}
+	
+		echo "hola";
 	
 	$query = new ParseQuery("lista");
 	$query->equalTo("mandado", "no");
@@ -39,7 +46,7 @@
 	}	
 	
 
-
+	echo "hola";
 	
 	
 	
